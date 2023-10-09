eab1837b31cc6ceccd0786ce8c8a4a3bef6d5d09
Carloslpbr <chreis1234@gmail.com>
2023-10-09 15:20:30 -0300

    Small_changes

--- pokemon_battle.py
+++ pokemon_battle.py
@@ -7,7 +7,6 @@ import settings
 import save_data
 
 # Basic
-
 player_1 = pokemon_select.set_pokemon()
 settings.any_char()
 basic_url = "https://pokeapi.co/api/v2/pokemon/"
@@ -18,15 +17,12 @@ output_file = os.path.join(current_directory, "battle_log.csv")
 #implementar leitura de txt aqui
 current_game = ...
 
-
 def generate_enemy():
-
-    # Generate num and export data
-    enemy_num = str(random.randint(0, 149))
+    pokemon_max_num = 149
+    enemy_num = str(random.randint(0, pokemon_max_num))
     enemy_url = requests.get(basic_url + enemy_num)
     json_enemy_data = enemy_url.json()
-    enemy_stats = pokemon_select.get_pStats(json_enemy_data)
-     
+    enemy_stats = pokemon_select.get_pStats(json_enemy_data)     
           
     eName = json_enemy_data['name']
     eHP = enemy_stats[0]
@@ -53,8 +49,14 @@ def player_stats():
 
     return pHP, pAP, pDF, pSA, pSD, pSP
 
-def battle_generator():
+def headers(battle_n,round_n, player_1, p1_hp, p2_name, p2_hp):
+    os.system('cls')
+    print(f"Batalha nº {battle_n}")
+    combat_param.display_turn(round_n,player_1)
+    combat_param.display_header(player_1,p1_hp)
+    combat_param.display_header(p2_name,p2_hp)
 
+def battle_generator():
     p1_hp, p1_ap, p1_df, p1_sa, p1_sd, p1_sp = player_stats() 
     battle_n = 1
     round_n = 1
@@ -64,19 +66,10 @@ def battle_generator():
     while battle_n < settings.combats:      
         p2_name, p2_hp, p2_ap, p2_df, p2_sa, p2_sd, p2_sp = generate_enemy()
 
-
         ##### Battle
-        while p1_hp > 0 and p2_hp > 0:
-            os.system('cls')  
-            print(f"Batalha nº: {battle_n}")     
+        while p1_hp > 0 and p2_hp > 0:              
             
-
-            ##### combat header
-            combat_param.display_turn(round_n,player_1)
-
-            ##### poke Headers
-            combat_param.display_header(player_1,p1_hp)
-            combat_param.display_header(p2_name,p2_hp)
+            headers(battle_n,round_n, player_1, p1_hp, p2_name, p2_hp)
 
             ##### Player turn
             print("Battle log:")
@@ -84,18 +77,15 @@ def battle_generator():
             pSpeed, pSpeed_p = combat_param.get_dodge_rate(player_1,p1_sp)
             pDef, pDef_p = combat_param.get_defense_power(player_1,p1_df)
 
-
             ##### mover bloco ?                      
             eSpeed, eSpeed_p = combat_param.get_dodge_rate(p2_name,p2_sp)      
             eDef, eDef_p = combat_param.get_defense_power(p2_name,p2_df)
             
             if power_result > eSpeed_p:
-                
                 p2_hp, damage = combat_param.attack(player_1, eDef, p2_hp, damage_done, result,p2_name)
                 damage_done += damage
 
                 if p2_hp <= 0:
-
                     save_data.save_data_w(output_file,player_1,p2_name,battle_n,round_n,damage_done,damage_taken)
                     break
                             
@@ -104,51 +94,31 @@ def battle_generator():
                 #colocar em função ?
                 print(f"{p2_name} Esquivou do seu ataque!")                
                 print("")
-                settings.any_char()
-            
+                settings.any_char()            
 
             os.system('cls')
 
-            ##### combat header
-            print(f"Batalha nº: {battle_n}")
-            combat_param.display_turn(round_n,p2_name)
-            
-            
-            ##### poke Headers            
-            combat_param.display_header(player_1,p1_hp)
-            combat_param.display_header(p2_name,p2_hp)
+            headers(battle_n,round_n, player_1, p1_hp, p2_name, p2_hp)
             settings.any_char() 
 
             ##### Enemy Turn
             combat_param.display_turn(round_n,p2_name)
             eAction, eResult, ePower_result = combat_param.enemy_action(p2_name,p2_ap)
-            
-            
 
-            ###dek
             if ePower_result > pSpeed_p:
-
                 p1_hp, damage_e = combat_param.attack(p2_name, pDef, p1_hp, damage_taken, eResult, player_1)
                 damage_taken += damage_e
 
                 if p1_hp <= 0:
                     save_data.save_data_l(output_file,player_1,p2_name,battle_n,round_n,damage_done,damage_taken)
-                    
                     break
                                 
-
             else:
-                #colocar em função ?
+                
                 print(f"{player_1} Esquivou do seu ataque!")                
                 print("")
                 settings.any_char()
 
-
-
-
             round_n += 1
-            
-
-   
-
+  
 battle_generator()
